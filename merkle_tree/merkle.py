73bc8817d9fd1728e599cbd05602d3c54a093205
Kariy <ammr.arf@gmail.com>
2023-12-30 11:38:59 +0800

    update

--- merkle_tree/merkle.py
+++ merkle_tree/merkle.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from hashlib import sha256
+import hashlib
+import numbers
 
 
 class Hasher(ABC):
@@ -16,30 +17,37 @@ class Sha256(Hasher):
     def __init__(self) -> None:
         self
 
-    def hash(self, string: bytes = b"") -> bytes:
-        return sha256(string).digest()
+    def hash(self, string: bytes) -> bytes:
+        return hashlib.sha256(string).digest()
 
 
-class MerkleTree:
-    """
-    A merkle tree with a generic hasher.
-    """
+"""
+Compute the merkle root of a list of leaves. Returning a list of bytes representing the merkle tree.
+"""
 
-    def __init__(self, hasher: Hasher = Sha256) -> None:
-        self.hasher = hasher
 
-    def build(self, leaves: list[bytes]) -> list[bytes]:
-        if len(leaves) % 2 == 1:
-            raise Exception("leaves must be of even length")
+def merkle_root(leaves: list[bytes], hasher: Hasher = Sha256()) -> list[bytes]:
+    if len(leaves) % 2 == 1:
+        raise Exception("leaves must be of even length")
 
-        tree = [0] * len(leaves) + leaves
-        for i in range(len(leaves) - 1, 0, -1):
-            tree[i] = self.hasher.hash(tree[i * 2] + tree[i * 2 + 1])
-        return tree
+    tree = [b""] * (len(leaves) - 1) + leaves
+    for i in range(len(leaves) - 1, -1, -1):
+        tree[i - 1] = hasher.hash(tree[i * 2 - 1] + tree[i * 2])
+    return tree
+
+
+def bytesToHex(byte):
+    if isinstance(byte, numbers.Number):
+        return hex(byte)
+    else:
+        return hex(
+            int.from_bytes(
+                byte,
+            )
+        )
 
 
 leaves = [i.to_bytes(32) for i in range(4)]
-tree = MerkleTree().build(leaves)
+tree = merkle_root(leaves)
 
-
-print(hex(int.from_bytes(tree[1])))
+print(list(map(bytesToHex, tree)))
